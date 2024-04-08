88b39fa8164dd815419ecef077e516ac814bca2e
Diana <diana.denisenko12@gmail.com>
2024-04-08 13:10:22 +0300

    Updated user model

--- order_proj/authenticate/urls.py
+++ order_proj/authenticate/urls.py
@@ -5,11 +5,9 @@ from . import views
 
 urlpatterns = [
     path('api/token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-   # path('accounts/login/', views.login_view, name='login'),
-    #path('accounts/logout/', views.LogoutView.as_view(), name='logout'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('register/', views.RegisterView.as_view(), name='auth_register'),
-    #Profile
+    path('api/register/', views.RegisterView.as_view(), name='auth_register'),
+    # Profile
     path('api/profile/', views.getProfile, name='profile'),
     path('api/profile/update/', views.updateProfile, name='update-profile'),
 ]
--- order_proj/authenticate/views.py
+++ order_proj/authenticate/views.py
@@ -1,89 +1,10 @@
-from datetime import timedelta, datetime
-
-from rest_framework_simplejwt.tokens import AccessToken
-from django.contrib.auth.password_validation import validate_password
-from django.shortcuts import render
-from rest_framework import generics, serializers, status
+from rest_framework import generics
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.validators import UniqueValidator
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from rest_framework_simplejwt.tokens import RefreshToken
-from django.contrib.auth import authenticate, login, logout
+from rest_framework_simplejwt.views import TokenObtainPairView
 from .models import CustomUser
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
-from django.http import JsonResponse
-from rest_framework_simplejwt.authentication import JWTAuthentication
-
-from django.contrib.auth import authenticate, login
-from django.shortcuts import render, redirect
-
-
-class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
-    @classmethod
-    def get_token(cls, user):
-        token = super().get_token(user)
-      #  jwt_authentication = JWTAuthentication()
-     #   jwt_authentication.authenticate(request=None, user=user)
-        # Add custom claims
-        token['username'] = user.username
-        # Get current time
-        current_time = datetime.utcnow()
-
-        # Check if access token has expired
-        if token['exp'] < current_time.timestamp():
-            # If token is expired, create a new refresh token
-            refresh = RefreshToken.for_user(user)
-            # Set a new expiration time for access token
-           # token.set_exp(timedelta(minutes=5))  # Set expiration time (e.g., 5 minutes)
-            # Return both access and refresh tokens
-            return {
-                'access': str(token),
-                'refresh': str(refresh),
-            }
-
-        return token
-
-
-class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(
-        write_only=True, required=True, validators=[validate_password])
-    password2 = serializers.CharField(write_only=True, required=True)
-    email = serializers.EmailField(
-        required=True,
-        validators=[UniqueValidator(queryset=CustomUser.objects.all())]
-    )
-
-    class Meta:
-        model = CustomUser
-        fields = ('username', 'email', 'password', 'password2', 'bio')
-
-    def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError(
-                {"password": "Password fields didn't match."})
-
-        return attrs
-
-    def create(self, validated_data):
-        user = CustomUser.objects.create(
-            username=validated_data['username'],
-            bio=validated_data['bio']
-        )
-
-        user.set_password((validated_data['password']))
-        user.save()
-
-        return user
-
-class ProfileSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = CustomUser
-        fields = '__all__'
-
-
+from .serializers import MyTokenObtainPairSerializer, RegisterSerializer, ProfileSerializer
 
 
 #Login User
@@ -95,8 +16,6 @@ class RegisterView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
-
-
 
 
 #api/profile  and api/profile/update
